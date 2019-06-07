c3611cdcbe0107c442becd6ae61cc457148ceea2
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-06-07 12:19:02 -0300

    Fix #30 - Corrigida chamada getenv

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -2,18 +2,18 @@
 
 return [
     'title'=> 'Pessoas',
-    'dashboard_url' => getenv(APP_URL),
+    'dashboard_url' => getenv('APP_URL'),
     'logout_method' => 'POST',
-    'logout_url' => getenv(APP_URL).'logout',
-    'login_url' => getenv(APP_URL).'login',
+    'logout_url' => getenv('APP_URL').'logout',
+    'login_url' => getenv('APP_URL').'login',
     'menu' => [
         [
             'text' => 'Busca por número USP',
-            'url'  => getenv(APP_URL).'buscas/codpes',
+            'url'  => getenv('APP_URL').'buscas/codpes',
         ],
         [
             'text' => 'Busca por nome',
-            'url'  => getenv(APP_URL).'buscas/nomepes',
+            'url'  => getenv('APP_URL').'buscas/nomepes',
         ],
     ]
 ];
