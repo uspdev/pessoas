02d6150bd9b39f85e73bc6e7db2576814983b8be
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-06-07 12:49:38 -0300

    Fix #31 - Corrigidas as rotas para as buscas

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -2,18 +2,18 @@
 
 return [
     'title'=> 'Pessoas',
-    'dashboard_url' => getenv('APP_URL'),
+    'dashboard_url' => '/' . getenv('APP_URL'),
     'logout_method' => 'POST',
-    'logout_url' => getenv('APP_URL').'logout',
-    'login_url' => getenv('APP_URL').'login',
+    'logout_url' => '/logout',
+    'login_url' => '/login',
     'menu' => [
         [
             'text' => 'Busca por número USP',
-            'url'  => getenv('APP_URL').'buscas/codpes',
+            'url'  => '/buscas/codpes',
         ],
         [
             'text' => 'Busca por nome',
-            'url'  => getenv('APP_URL').'buscas/nomepes',
+            'url'  => '/buscas/nompes',
         ],
     ]
 ];
