c071021261ce3764407f967b1dd6f19731c2935a
Arthur Antunes Ferreira <arthurferreira@usp.br>
2020-06-23 00:36:39 -0300

    Model e migrate parcialmente implementados

---
+++ database/migrations/2020_06_23_025921_create_campos_extras_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCamposExtrasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campos_extras', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('numero_usp');
+            $table->string('nome');
+            $table->date('data_nascimento')->nullable();
+            $table->string('sexo')->nullable();
+            $table->string('pai')->nullable();
+            $table->string('mae')->nullable();
+            $table->text('endereco')->nullable();
+            $table->string('cep')->nullable();
+            $table->string('cidade')->nullable();
+            $table->string('uf')->nullable();
+            $table->string('pais')->nullable();
+            $table->string('nacionalidade')->nullable();
+            $table->string('telefone')->nullable();
+            $table->string('celular')->nullable();
+            $table->string('email_extra')->nullable();
+            $table->string('cpf')->nullable();
+            $table->string('rg')->nullable();
+            $table->string('passaporte')->nullable();
+            $table->date('validade_visto')->nullable();
+            $table->string('rne')->nullable();
+            $table->string('pis')->nullable();
+            $table->string('lotado')->nullable();
+            $table->string('banco')->nullable();
+            $table->string('agencia')->nullable();
+            $table->string('conta_corrente')->nullable();
+            $table->string('nome_sigla_uni_vinculo')->nullable();
+            $table->boolean('docente_usp')->nullable();
+            $table->string('empenho')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campos_extras');
+    }
+}
