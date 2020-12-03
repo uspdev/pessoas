3a3e60a581fdeb7dfe9c0de419969402d3fa83c3
Masaki Kawabata Neto <kawabata@usp.br>
2020-12-03 11:00:39 -0300

    atualizando theme v2

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -2,19 +2,20 @@
 
 return [
     'title' => config('app.name'),
-    'dashboard_url' => config('app.url'),
+    'app_url' => config('app.url'),
+    'skin' => env('USP_THEME_SKIN', 'uspdev'),
     'logout_method' => 'POST',
-    'logout_url' => config('app.url') . '/logout',
-    'login_url' => config('app.url') . '/login',
+    'logout_url' => 'logout',
+    'login_url' => 'login',
     'menu' => [
         [
             'text' => 'Busca',
-            'url'  =>  config('app.url') . '/search',
+            'url'  =>  'search',
             'can'  => 'admin'
         ],
         [
             'text' => 'Pessoas Autorizadas',
-            'url'  => config('app.url') . '/users',
+            'url'  => 'users',
             'can'  => 'admin'
         ],
     ]
