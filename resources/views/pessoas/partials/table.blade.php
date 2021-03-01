2a67183e7f01261eeca67112087a4a6f1503f0b1
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2021-03-01 11:37:58 -0300

    Adicionadas as colunas Vínculos e Setores nos resultados da busca por nome

--- resources/views/pessoas/partials/table.blade.php
+++ resources/views/pessoas/partials/table.blade.php
@@ -1,16 +1,22 @@
 <table class="table">
   <thead class="thead-light">
     <tr>
+      <th scope="col">Nº</th>
       <th scope="col">Número USP</th>
       <th scope="col">Nome</th>
+      <th scope="col">Vínculos</th>
+      <th scope="col">Setores</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      @foreach($pessoas as $pessoa)
+      @foreach($pessoas as $index => $pessoa)
       <tr>
-        <th scope="row">{{$pessoa['codpes']}}</th>
+        <td scope="row">{{$index + 1}}</td>
+        <td scope="row">{{$pessoa['codpes']}}</td>
         <td><a href="pessoas/{{$pessoa['codpes']}}">{{$pessoa['nompes']}}</a></td>
+        <td>@if ($pessoa['codundclg']) {{implode(',', \Uspdev\Replicado\Pessoa::vinculosSiglas($pessoa['codpes'], $pessoa['codundclg']))}} @endif</td>
+        <td>@if ($pessoa['codundclg']) {{implode(',', \Uspdev\Replicado\Pessoa::setoresSiglas($pessoa['codpes'], $pessoa['codundclg']))}} @endif</td>
       </tr>
       @endforeach
 
