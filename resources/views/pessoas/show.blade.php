56e6e74084bc16123413b8d813c90ababa1d48bc
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-05-13 11:19:32 -0300

    mantém a caixa de busca vísivel

--- resources/views/pessoas/show.blade.php
+++ resources/views/pessoas/show.blade.php
@@ -1,12 +1,8 @@
 @extends('laravel-usp-theme::master')
 
-@section('content_header')
-<h1></h1>
-@stop
-
 @section('content')
 @include('alerts')
-
+@include('pessoas.partials.search')
 @include('pessoas.partials.show.replicado')
 <br>
 @include('pessoas.partials.show.campos_extras')
