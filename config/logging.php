bc0466417326077f64cffd0e99cc33b8c8bb489f
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-06-01 16:54:12 -0300

    Configurando stack para enviar logs para syslog

--- config/logging.php
+++ config/logging.php
@@ -32,7 +32,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single','syslog'],
         ],
 
         'single' => [
