1db2bf64c11864d0d53a4dd4669625338d28bf47
Kawan Santana <kawansantana18@gmail.com>
2024-04-22 09:11:47 -0300

    Add função e setor na listagem

--- resources/views/designados/index.blade.php
+++ resources/views/designados/index.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
     <h2>Lista de Servidores Designados</h2>
-    <table class="table table-striped table-sm datatable ">
+    <table class="table table-striped table-sm table-designados">
         <thead>
             <tr>
                 <th>N° USP</th>
                 <th>Nome</th>
+                <th>Função</th>
+                <th>Setor</th>
                 <th>E-mail</th>
                 <th>Telefone</th>
             </tr>
@@ -16,10 +18,25 @@
                 <tr>
                     <td>{{ $designado['codpes'] }}</td>
                     <td><a href="{{route('pessoas.show', $designado['codpes'])}}">{{ $designado['nompesttd'] }}</a></td>
+                    <td>{{ $designado['nomfnc'] }}</td>
+                    <td>{{ $designado['nomabvset'] }}</td>
                     <td>{{ $designado['codema'] }}</td>
                     <td>{{ $designado['numtelfmt'] }}</td>
                 </tr>
             @endforeach
         </tbody>
     </table>
+@endsection
+
+@section('javascripts_bottom')
+<script>
+    $(document).ready(function(){
+    
+        new DataTable('.table-designados', {
+            order: [[1, 'asc']],
+            iDisplayLength: 100
+        });
+
+    });
+</script>
 @endsection
