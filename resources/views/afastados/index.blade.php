1af2f4ef092a34cc1cfea4e5e11d15e61dc5d9fe
Kawan Santana <kawansantana18@gmail.com>
2024-04-23 16:19:44 -0300

    Trocando ordem das colunas e-mail e telefones

--- resources/views/afastados/index.blade.php
+++ resources/views/afastados/index.blade.php
@@ -8,10 +8,10 @@
             <th>Nome</th>
             <th>Setor</th>
             <th>Motivo do Afastamento</th>
-            <th>E-mail</th>
-            <th>Telefones</th>
             <th>Data de Início</th>
             <th>Data de Término</th>
+            <th>E-mail</th>
+            <th>Telefones</th>
         </thead>
         <tbody>
             @foreach ($afastados as $afastado)
@@ -20,10 +20,10 @@
                <td><a href="{{route('pessoas.show', $afastado['codpes'])}}">{{$afastado['nompes']}}</a></td> 
                <td>{{$afastado['nomabvset']}}</td> 
                <td>{{$afastado['sitoco']}}</td> 
-               <td>{{$afastado['codema']}}</td> 
-               <td>{{implode(" / ", $afastado['telefones'])}}</td> 
                <td>{{date('d/m/Y', strtotime($afastado['dtainisitoco']))}}</td> 
                <td>{{date('d/m/Y', strtotime($afastado['dtafimsitoco']))}}</td> 
+               <td>{{$afastado['codema']}}</td> 
+               <td>{{implode(" / ", $afastado['telefones'])}}</td> 
             </tr>
             @endforeach
         </tbody>
